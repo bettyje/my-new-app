46bb0e88396dd2130e9ecbdeec9a55c7555eb025
bettyje <bettyjelagat156@gmail.com>
2025-02-04 12:32:22 +0300

    modified

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
 import './JournalPage.css'
 
 const Journal = ({ user }) => {
